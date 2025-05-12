b5ab0ef7ca3f21fc75ca6d19f6daec2f9df40099
Tanelo Mondaca <mondaca.tanelo@seiza-ti.cl>
2025-05-11 23:40:39 -0400

    FEAT | Estilo para footer con totales

---
+++ src/app/components/summary-totals/summary-totals.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatListModule} from '@angular/material/list';
+
+@Component({
+  selector: 'app-summary-totals',
+  imports: [MatListModule],
+  templateUrl: './summary-totals.component.html',
+  styleUrl: './summary-totals.component.css'
+})
+export class SummaryTotalsComponent {
+
+}
